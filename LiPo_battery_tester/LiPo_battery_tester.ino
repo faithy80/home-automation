f24b75c449682380a686532a005120ab7e8a9bc0
faithy80 <53244256+faithy80@users.noreply.github.com>
2020-10-01 21:56:05 +0100

    LiPo battery test code added

---
+++ LiPo_battery_tester/LiPo_battery_tester.ino
@@ -0,0 +1,24 @@
+/* This is a simple tester application to determine the voltage the connected LiPo battery
+ */
+
+#define LiPo_pin A0               // Define the battery measuring pin
+
+void setup()
+{
+  pinMode(LiPo_pin, INPUT);       // Set the measuring pin to input
+  Serial.begin(9600);             // Open the srial port for debug
+}
+
+void loop() {
+  // Get the analog voltage converted to units (~ 0V - 5V ==> 0 - 1023 units)
+  // For example, 4.12V is 905 units while the Arduino Nano is powered from USB
+  // I need to recalibrate the voltages when the device is operated from battery
+  int reading = analogRead(0);
+
+  // Print reading to the serial port
+  Serial.print(reading);
+  Serial.println(" units");
+  
+  // Wait 1 second before the next reading
+  delay(1000);
+}
