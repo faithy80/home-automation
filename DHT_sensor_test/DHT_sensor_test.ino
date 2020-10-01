d6f659b3c6b0c50922c050831cbd54e16b17f3cf
faithy80 <53244256+faithy80@users.noreply.github.com>
2020-10-01 14:39:59 +0100

    DHT sensor test code added

---
+++ DHT_sensor_test/DHT_sensor_test.ino
@@ -0,0 +1,39 @@
+#include "DHT.h"              // Include the DHT temperature and humidity sensor library
+#define DHTPIN 2              // Select the pin that the sensor is connected to
+#define DHTTYPE DHT11         // Select the type of the sensor
+DHT dht(DHTPIN, DHTTYPE);     // Create an instance of the DHT object
+
+
+void setup()
+{
+  Serial.begin(9600);         // Start the serail port for debug
+  dht.begin();                // Start the temperature sensor
+}
+
+
+void loop()
+{
+  // Read the temperature and humidity
+  float humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  
+  // Test if the reading was successful
+  if (isnan(temperature) || isnan(humidity))
+  {
+    Serial.println("Incorrect values!");
+  }
+
+  else
+  // Send the temperature and the humidity data to the serial port
+  {
+    Serial.print("Humidity: ");
+    Serial.print(humidity);
+    Serial.print("%\t");
+    Serial.print("Temperature: ");
+    Serial.print(temperature);
+    Serial.println("C");
+  }
+
+  // Set delay for 10 seconds
+  delay(10000);
+}
